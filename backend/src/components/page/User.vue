c2f8e16d7935472443bf94aa34cf19fcdfa81e55
jay <1627158922@qq.com>
2023-04-03 14:52:45 +0800

    添加Web端页面

---
+++ backend/src/components/page/User.vue
@@ -0,0 +1,22 @@
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
+
+<template>
+  <div class="user">
+    <el-breadcrumb>
+      <el-breadcrumb-item :to="'/'">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <div class="main">
+        dsds
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+
+  }
+</script>
